4bc78c34a2a097851bf6d1c2c066b8d9bbe9d6f5
Kevin Connell <116701236+KevConn22@users.noreply.github.com>
2023-07-10 14:42:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tkinter import *
 from tkinter import ttk
+import csv
 
 """
 For reference, this code was created with help from a variety of sources. Due to my lack of prior experience with Tkinter, a lot of helpful information was found at tkdocs.com/tutorial, including the use of some prior-developed code in the tutorial. Furthermore, a few of my questions were answered via StackOverflow browsing, and several other forums helped fill in the gaps where necessary.
@@ -56,6 +57,68 @@ def remove_leading_zeros(list_characters):
     else:
       break
 
+#Function for the purging of different lists [See create_dif_list for use]
+def purge_list(list1, list2, list3):
+  for item in list1:
+    for i in range(len(list2)):
+      if i == item:
+        list3.append(list2[i])
+        
+#Creates the list of correct pn's, but different quantities (returns list of part number and quantity in both Inventor and IFS)
+#Looks bad, but is fully functional. Returns list of lists, giving PN, Qty in Inv, and IFS QTY
+def create_dif_list(f1, f2):
+  inv_qty_idx = []
+  ifs_qty_idx = []
+  dI= pd.read_csv(f1, usecols=['Part Number', 'Description', 'QTY'])
+  dIFS = pd.read_csv(f2, usecols=['Part Number', 'Part Description', 'Quantity  Per Assembly'])
+  inventor_pn = dI["Part Number"].values.tolist()
+  inventor_qty = dI["QTY"].values.tolist()
+  ifs_pn = dIFS["Part Number"].values.tolist()
+  ifs_qty = dIFS["Quantity  Per Assembly"].values.tolist()
+  for i in range(len(inventor_pn)):
+    number_list = list(inventor_pn[i])
+    remove_leading_zeros(number_list)
+    new_number = ''.join(number_list)
+    inventor_pn[i] = new_number
+  common = set(inventor_pn).intersection(set(ifs_pn))
+  
+  #Two sets of code that copy over indicies of matching part numbers over...very helpful
+  for item in common:
+    for i in range(len(inventor_pn)):
+      if inventor_pn[i] == item:
+        inv_qty_idx.append(i)
+  for item in common:
+    for i in range(len(ifs_pn)):
+      if ifs_pn[i] == item:
+        ifs_qty_idx.append(i)
+
+  #Creates the list of only those part numbers and quantities
+  inv_pn_final = []
+  inv_qty_final = []
+  ifs_pn_final = []
+  ifs_qty_final = []  
+
+  #Purges all lists (PN and QTY) of unique values, leaving only common ones behind
+  purge_list(inv_qty_idx, inventor_pn, inv_pn_final)
+  purge_list(inv_qty_idx, inventor_qty, inv_qty_final)
+  purge_list(ifs_qty_idx, ifs_pn, ifs_pn_final)
+  purge_list(ifs_qty_idx, ifs_qty, ifs_qty_final)
+
+  #Creates master list of tuples containing part number and qty for each common part
+  inv_master = []
+  for i in range(len(inv_pn_final)):
+    inv_master.append((inv_pn_final[i], inv_qty_final[i]))
+  ifs_master = []
+  for i in range(len(ifs_pn_final)):
+    ifs_master.append((ifs_pn_final[i], ifs_qty_final[i]))
+
+  list_differences = []
+  
+  for i in range(len(inv_master)):
+    for j in range(len(ifs_master)):
+      if inv_master[i][0] == ifs_master[j][0] and inv_master[i][1] != ifs_master[j][1]:
+        list_differences.append([inv_master[i][0], inv_master[i][1], ifs_master[j][1]])
+    
 #Removes leading zeros from inventor_pn list (CSV from IFS does so, so must do so here)
 #OBSELETE as of 7/10/23; implemented directly into "compare" function
 def update_list_removed_zeros():
@@ -84,13 +147,32 @@ def compare(*args):
   ifs.set(ifs_df)
 
 #Function that handles the comparison and subsequent export of the p/n and qty data
-def export():
-  pass 
+def export(*args):
+  #Copy of code from compare in order to properly pass the information to the comparison list/dataframe
+  inv_list = create_inv_list(inv_filename.get() + ".csv")
+  ifs_list = create_ifs_list(ifs_filename.get() + ".csv")
+  for i in range(len(inv_list)):
+    number_list = list(inv_list[i])
+    remove_leading_zeros(number_list)
+    new_number = ''.join(number_list)
+    inv_list[i] = new_number
+  common = set(inv_list).intersection(set(ifs_list))
+  inv_list = [i for i in inv_list if i not in common]
+  ifs_list = [i for i in ifs_list if i not in common]
+  inventor_df = pd.DataFrame(inv_list, columns=['Part Number'])
+  ifs_df = pd.DataFrame(ifs_list, columns=['Part Number'])
+  inventor.set(inventor_df)
+  ifs.set(ifs_df)
+  #New code for writing into new local CSV file
+  #Change to offer implementation to select the file location with Windows Explorer at home
+  inventor_csv = inventor_df.to_csv("Inventor-Specific Deltas.csv", sep='\t')
+  ifs_csv = ifs_df.to_csv("IFS-Specific Deltas.csv", sep='\t')
+  
 
 #Creates the GUI using Tkinter
 root = Tk()
 root.title("Inventor v. IFS BOM Comparison")
-root.geometry("380x400")
+root.geometry("340x400")
 
 #Sets basic mainframe grid to be used in initial screen
 mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -109,10 +191,10 @@ inv_filename_entry = ttk.Entry(mainframe, width=12, textvariable=ifs_filename)
 inv_filename_entry.grid(column=2, row=3, sticky=(W, E))
 
 #Creates the submit button (hate this button.)
-ttk.Button(mainframe, text="Compare", command=compare).grid(column=2, row=4, sticky=W)
+ttk.Button(mainframe, text="Quick Compare", command=compare).grid(column=2, row=4, sticky=W)
 
 #Creates the labels for IFS and Inventor filename submission boxes
-ttk.Label(mainframe, text="Inventor BOM Filename: ").grid(column=1, row=2, sticky=W)
+ttk.Label(mainframe, text="Inventor BOM Filename: ").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="IFS BOM Filename: ").grid(column=1, row=3, sticky=E)
 
 #Creates labels for output categorization
@@ -127,7 +209,7 @@ ttk.Label(mainframe, textvariable=inventor).grid(column=1, row=6, sticky= (N))
 ttk.Label(mainframe, textvariable=ifs).grid(column=2, row=6, sticky=(N))
 
 #Creates button for export to external file
-ttk.Button(mainframe, text="Export to CSV", command=export).grid(column=1, row=4, sticky=E)
+ttk.Button(mainframe, text="Compare/Export to CSV", command=export).grid(column=1, row=4, sticky=E)
 
 #Makes it look more aesthetic by adding padding around each grid element
 for child in mainframe.winfo_children(): 
